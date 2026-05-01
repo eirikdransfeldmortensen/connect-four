016842f8445e46904aa4d594ee914fff8ecceff5
eirikdransfeldmortensen <eirik.dransfeld.mortensen@gmail.com>
2026-05-01 21:25:21 +0200

    built gamebody

---
+++ src/game.py
@@ -0,0 +1,53 @@
+#import pygame
+
+
+class Board:
+    
+    def __init__(self):
+        self.num_rows = 6
+        self.num_cols = 7
+        self.grid = [[0 for _ in range(self.num_cols)] for _ in range(self.num_rows)]
+
+    def print_board(self):
+        for row in self.grid:
+          print(row)
+    
+    def drop_piece(self, column, current_player):
+        for row in range(self.num_rows - 1, -1, -1,):
+            if self.grid[row][column] == 0:
+                self.grid[row][column] = current_player
+                return True
+        return False
+    
+    def check_win(self, current_player):
+        for row in range(self.num_rows):
+            for col in range(self.num_cols -3):
+               if (self.grid[row][col] == current_player and
+                self.grid[row][col+1] == current_player and
+                self.grid[row][col+2] == current_player and
+                self.grid[row][col+3] == current_player):
+                return True
+
+        for row in range(self.num_rows - 3):
+            for col in range(self.num_cols):
+                if (self.grid[row][col] == current_player and
+                    self.grid[row+1][col] == current_player and
+                    self.grid[row+2][col] == current_player and
+                    self.grid[row+3][col] == current_player):
+                    return True
+        for row in range(self.num_rows - 3):
+            for col in range(self.num_cols - 3):
+                if (self.grid[row][col] == current_player and
+                    self.grid[row+1][col+1] == current_player and
+                    self.grid[row+2][col+2] == current_player and
+                    self.grid[row+3][col+3] == current_player):
+                    return True
+
+        for row in range(3, self.num_rows):
+            for col in range(self.num_cols - 3):
+                if (self.grid[row][col] == current_player and
+                    self.grid[row-1][col+1] == current_player and
+                    self.grid[row-2][col+2] == current_player and
+                    self.grid[row-3][col+3] == current_player):
+                    return True
+        return False
