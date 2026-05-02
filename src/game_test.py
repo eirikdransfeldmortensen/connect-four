6f999c7aa95f2ee900b7ac51e86531216073c3b6
eirikdransfeldmortensen <eirik.dransfeld.mortensen@gmail.com>
2026-05-02 21:27:53 +0200

    finished

--- src/game_test.py
+++ src/game_test.py
@@ -9,12 +9,12 @@ class Board:
 
     def print_board(self):
         for i in range(0, 42, 7):
-            print(f"  {self.board[i]}  |  {self.board[i+1]}  |  {self.board[i+2]}  |  {self.board[i+3]}  |  {self.board[i+4]}  |  {self.board[i+5]}  |  {self.board[i+6]} ")
+            print(f"\n  {self.board[i]}  |  {self.board[i+1]}  |  {self.board[i+2]}  |  {self.board[i+3]}  |  {self.board[i+4]}  |  {self.board[i+5]}  |  {self.board[i+6]} ")
             if i < 35:
                 print("-" * 41)
 
-    def available_grid_positions(self):
-        return [i for i, spot in enumerate(self.board) if spot == " "]
+    def available_columns(self):
+        return [col for col in range(7) if self.board[col] == " "]
     
     def drop_piece(self, column, player):
         for row in range(5, -1, -1):
@@ -35,20 +35,20 @@ class Board:
             #check rows
             if i % 7 <= 3:
                 if self.board[i] == self.board[i + 1] == self.board[i + 2] == self.board[i + 3] != " ":
-                    print(f"{self.board[i]}, You've won!")
+                    return self.board[i]
             
             #check column
-            if i + 27 < 42:
+            if i + 21 < 42:
                 if self.board[i] == self.board[i + 7] == self.board[i + 14] == self.board[i + 21] != " ":
-                    print(f"{self.board[i]}, You've won!")
+                    return self.board[i]
                 
             #check diagonals
             if i % 7 <= 3 and i + 24 < 42:
                 if self.board[i] == self.board[i + 8] == self.board[i + 16] == self.board[i + 24] != " ":
-                    print(f"{self.board[i]}, You've won!")
+                    return self.board[i]
             if i % 7 >= 3 and i + 18 < 42:
                 if self.board[i] == self.board[i + 6] == self.board[i + 12] == self.board[i + 18] != " ":
-                    print(f"{self.board[i]}, You've won!")
+                    return self.board[i]
         
         return None
     
@@ -56,36 +56,105 @@ class Board:
     def game_over(self):
         return self.check_winner() is not None or self.is_board_full()
     
-    def minimax(self, depth, is_maximizing):
+    def minimax(self, depth, is_maximizing, max_depth = 9, alpha=float("-inf"), beta=float("inf")):
         winner = self.check_winner()
         if winner == self.ai_player:
-            return 1
+            return 10 - depth
         if winner == self.human_player:
-            return -1
+            return depth - 10
         if self.is_board_full():
+            return 0
+        if depth >= max_depth:
             return 0
 
         if is_maximizing:
             best_score = float("-inf")
-            for move in self.available_grid_positions():
-                self.board[move] = self.ai_player
-                score = self.minimax(depth + 1, False)
-                self.board[move] = " "
+            for col in self.available_columns():
+                self.drop_piece(col, self.ai_player) 
+                score = self.minimax(depth + 1, False, max_depth, alpha, beta)
+                self.undo_drop(col)
+                best_score = max(score, best_score)
+                alpha = max(alpha, best_score)
+                if beta <= alpha:
+                    break
+            return best_score
+        
+        else:
+            #if next turn is human, we want to minimize their score
+            best_score = float("inf")
+            for col in self.available_columns():
+                self.drop_piece(col, self.human_player)
+                score = self.minimax(depth + 1, True, max_depth, alpha, beta)
+                self.undo_drop(col)
+                best_score  = min(score, best_score)
+                beta = min(beta, best_score)
+                if beta <= alpha:
+                    break
+            return best_score
+    
+    def undo_drop(self, column):
+        for row in range(6):
+            index = row * 7 + column
+            if self.board[index] != " ":
+                self.board[index] = " "
+                return
+        
+    
+    def get_best_col(self):
+        import random
+        best_score = float("-inf")
+        best_cols = []
 
+        for col in self.available_columns():
+            print(f"\nEvaluating column {col}...")
+            self.drop_piece(col, self.ai_player)
+            score = self.minimax(0, False)
+            self.undo_drop(col)
+            print(f"column {col} scored: {score}")
+            if score > best_score:
+                best_score = score
+                best_cols = [col]
+            elif score == best_score:
+                best_cols.append(col)
+                print(f"AI chose {col}")
 
-board = Board()
+        return random.choice(best_cols)
+    
+    def play_game(self):
+        import random
+        ai_turn = random.choice([True, False])
+        print("\nSetting up board...")
+        while not self.game_over():
+            self.print_board()
 
-board.drop_piece(0, "X")
-board.drop_piece(0, "X")
-board.drop_piece(0, "X")
-board.drop_piece(0, "X")
-board.drop_piece(1, "O")
+            if ai_turn:
+                print("\n AI's turn...")
+                col = self.get_best_col()
+                self.drop_piece(col, self.ai_player)
+            
+            else:
+                while True:
+                    try:
+                        col = int(input("\nChoose column (0-6): "))
+                        if 0 <= col <= 8 and self.drop_piece(col, self.human_player):
+                            break
+                        else:
+                            print("Invalid move! Try again.")
 
+                    except ValueError:
+                        print("Please enter a number between 0 and 8")
 
-board.print_board()
+            ai_turn = not ai_turn
 
+        self.print_board()
+        winner = self.check_winner()
+        if winner == self.ai_player:
+            print("\nAI wins!")
+        elif winner == self.human_player:
+            print("\nCongratulations! You win!")
+        else:
+            print("\nIt's a tie!")
 
-check = board.available_grid_positions()
-board.check_winner()
-     
-print(check)
+if __name__ == "__main__":
+    game = Board()
+    game.play_game()
--- src/main_test.py
+++ src/main_test.py
@@ -2,12 +2,39 @@ from game_test import Board
 
 board = Board()
 
-while True:
-    board.print_board()
-    col_pla = int(input("Choose column (0-6): "))
-    if not board.drop_piece(col_pla, board.human_player):
-        print("Column is full, pick another!")
+class Game:
 
-    if col_pla < 0 or col_pla > 6:
-        print("\nInvalid column, choose between 0 and 6.")
-        continue
+    def play_game(self):
+        import random
+        ai_turn = random.choice([True, False])
+
+        while not self.game_over():
+            board.print_board()
+
+            if ai_turn:
+                print("\n AI's turn...")
+                col = board.get_best_move()
+                board.drop_piece(col, board.ai_player)
+            
+            else:
+                while True:
+                    try:
+                        col = int(input("Choose column (0-6): "))
+                        if 0 <= col <= 8 and board.drop_piece(col, board.human_player):
+                            break
+                        else:
+                            print("Invalid move! Try again.")
+
+                    except ValueError:
+                        print("Please enter a number between 0 and 8")
+
+            ai_turn = not ai_turn
+
+        board.print_board()
+        winner = board.check_winner()
+        if winner == board.ai_player:
+            print("\nAI wins!")
+        elif winner == board.human_player:
+            print("\nCongratulations! You win!")
+        else:
+            print("\nIt's a tie!")
